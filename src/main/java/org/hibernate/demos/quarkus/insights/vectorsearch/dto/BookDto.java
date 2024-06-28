3bf8b71e0ff0c18e8587c4a53bef15e7e52fc604
Yoann Rodière <yoann@hibernate.org>
2024-06-28 15:54:54 +0200

    Use records for DTO

--- src/main/java/org/hibernate/demos/quarkus/insights/vectorsearch/dto/BookDto.java
+++ src/main/java/org/hibernate/demos/quarkus/insights/vectorsearch/dto/BookDto.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.hibernate.demos.quarkus.insights.vectorsearch.domain.Book;
 import org.hibernate.demos.quarkus.insights.vectorsearch.domain.Genre;
 import org.hibernate.search.engine.search.common.ValueConvert;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FieldProjection;
@@ -12,31 +11,14 @@ import org.hibernate.search.mapper.pojo.mapping.definition.annotation.HighlightP
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.IdProjection;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.ProjectionConstructor;
 
-public class BookDto extends Identifiable<Long> {
-
-	private String title;
-
-	private String summary;
-
-	private Identifiable<Long> author;
-
-	private Set<Genre> genres;
-
-	private String coverLocation;
-
-	public BookDto() {
-	}
-
-	public BookDto(Book book) {
-		this.id = book.id;
-		this.title = book.title;
-		this.summary = book.summary;
-		this.genres = book.genres;
-		this.coverLocation = book.coverLocation.getFileName().toString();
-		this.author = new AuthorDto( book.author );
-
-	}
-
+public record BookDto(
+		Long id,
+		String title,
+		String summary,
+		AuthorDto author,
+		String coverLocation,
+		Set<Genre> genres
+) implements Identifiable<Long> {
 	@ProjectionConstructor
 	public BookDto(
 			@IdProjection Long id,
@@ -45,53 +27,8 @@ public class BookDto extends Identifiable<Long> {
 			@FieldProjection(path = "author.name", convert = ValueConvert.NO) String author,
 			@FieldProjection(convert = ValueConvert.NO) String coverLocation,
 			@FieldProjection List<Genre> genres) {
-		this.id = id;
-		this.title = title.get( 0 );
-		this.summary = summary.get( 0 );
-		this.coverLocation = coverLocation;
-		AuthorDto author1 = new AuthorDto();
-		author1.setName( author );
-		this.author = author1;
-		this.genres = new HashSet<>( genres );
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getSummary() {
-		return summary;
-	}
-
-	public void setSummary(String summary) {
-		this.summary = summary;
-	}
-
-	public Identifiable<Long> getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(Identifiable<Long> author) {
-		this.author = author;
-	}
-
-	public Set<Genre> getGenres() {
-		return genres;
-	}
-
-	public void setGenres(Set<Genre> genres) {
-		this.genres = genres;
-	}
-
-	public String getCoverLocation() {
-		return coverLocation;
-	}
-
-	public void setCoverLocation(String coverLocation) {
-		this.coverLocation = coverLocation;
+		this( id, title.get( 0 ), summary.get( 0 ), new AuthorDto( null, author ),
+				coverLocation, new HashSet<>( genres )
+		);
 	}
 }
